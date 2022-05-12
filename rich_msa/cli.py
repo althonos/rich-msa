c841b70ec342f33a559c2c3eeed08bd769826d70
Martin Larralde <martin.larralde@embl.de>
2022-05-12 13:16:31 +0200

    Allow passing a Gecos color file to the `rich-msa` CLI

--- rich_msa/cli.py
+++ rich_msa/cli.py
@@ -1,9 +1,12 @@
 import argparse
+import json
 import os
 import typing
 
 import Bio.AlignIO
 import rich
+from rich.color import Color
+from rich.style import Style
 from rich.console import Console
 
 from . import RichAlignment
@@ -45,16 +48,32 @@ def main(argv: typing.Optional[typing.List[str]] = None, console: rich.console.C
         default=10,
         help="The maximum width of the name column"
     )
+    parser.add_argument(
+        "-c",
+        "--colors",
+        help="The path to a JSON file in Gecos format with the color scheme to use"
+    )
     args = parser.parse_args(argv)
 
     console = console if console is not None else Console()
     try:
+        # load style if any
+        if args.colors:
+            with open(args.colors, "rb") as colors_file:
+                data = json.load(colors_file)
+                styles = {
+                    letter:rich.style.Style(bold=True, color=Color.parse(color))
+                    for letter, color in data["colors"].items()
+                }
+        else:
+            styles = None
         # load MSA using Biopython
         msa = Bio.AlignIO.read(args.input, args.format)
         viewer = RichAlignment(
             names=[record.id for record in msa],
             sequences=[str(record.seq) for record in msa],
             max_name_width=args.max_name_width,
+            styles=styles,
         )
         # render the MSA
         panel = rich.panel.Panel(viewer, title=os.path.basename(args.input))
